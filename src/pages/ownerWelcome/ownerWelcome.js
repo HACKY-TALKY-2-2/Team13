3a5731fb362cc436e28e369f2a21fd6a54bddb45
ohwphil <ohwphil@gmail.com>
2023-11-11 03:09:07 +0900

    add owner login & register pages

--- src/pages/ownerWelcome/ownerWelcome.js
+++ src/pages/ownerWelcome/ownerWelcome.js
@@ -1,4 +1,23 @@
+import styled from "styled-components";
+import HomeButton from "../../components/homeButton";
+
 const OwnerWelcome = () => {
-  return <></>;
+  return (
+    <>
+      <HomeButton />
+      <BackgroundCircle />
+    </>
+  );
 };
 export default OwnerWelcome;
+
+const BackgroundCircle = styled.div`
+  background-image: url("Ellipse 1.svg");
+  background-size: cover; // This will make the image cover the entire container
+  background-position: center; // This will center the image
+  width: 70vh;
+  height: 70vh;
+  position: fixed;
+  top: 15vh;
+  right: 64px;
+`;
