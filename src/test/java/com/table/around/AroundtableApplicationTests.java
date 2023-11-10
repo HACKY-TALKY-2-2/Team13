757ef685522745a37720d6419af7a64fe2d14c6d
10Hyuna <kimha105@naver.com>
2023-11-10 22:47:44 +0900

    feat: init

---
+++ src/test/java/com/table/around/AroundtableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.table.around;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AroundtableApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
