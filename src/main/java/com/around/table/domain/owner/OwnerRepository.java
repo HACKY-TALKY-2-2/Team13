d0b0cc79fe566c24dd18ef90bb25a914e089b191
10Hyuna <kimha105@naver.com>
2023-11-11 04:34:59 +0900

    #5 test: signUp is successful

--- src/main/java/com/around/table/domain/owner/OwnerRepository.java
+++ src/main/java/com/around/table/domain/owner/OwnerRepository.java
@@ -4,5 +4,7 @@ import com.around.table.entity.Owner;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OwnerRepository extends JpaRepository<Owner, Long> {
+
     Owner findByOwnerId(String owerId);
+
 }
